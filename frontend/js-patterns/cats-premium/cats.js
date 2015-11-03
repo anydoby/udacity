b5f409f496ce21dac06f06c63223ac61a1fd5812
anydoby <anydoby@gmail.com>
2015-11-03 20:58:46 +0100

    adding everything

---
+++ frontend/js-patterns/cats-premium/cats.js
@@ -0,0 +1,6 @@
+var catsData = [
+                {
+                  src:'https://www.google.nl/search?q=cat+images&espv=2&biw=1440&bih=775&site=webhp&tbm=isch&imgil=krrt7OEGWrXF5M%253A%253BPMsFcGR6P7pjeM%253Bhttp%25253A%25252F%25252Fwww.playbuzz.com%25252Fabbeymcneill10%25252Fwhich-cat-breed-are-you&source=iu&pf=m&fir=krrt7OEGWrXF5M%253A%252CPMsFcGR6P7pjeM%252C_&usg=__xgd5ApsEaWo46btRW6JwOYjxI5c%3D',
+                  title: ''
+                }
+];
