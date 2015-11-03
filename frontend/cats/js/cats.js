73828136587294c56fd7fe7f556e2d02c43e8fe3
anydoby <anydoby@gmail.com>
2015-11-03 22:55:16 +0100

    started working on cats premium pro

--- frontend/cats/js/cats.js
+++ frontend/cats/js/cats.js
@@ -1,23 +1,83 @@
-var cats = [
-    {
-        title:'Cat face', src: 'http://www.google.nl/imgres?imgurl=http://dreamatico.com/data_images/cat/cat-6.jpg&imgrefurl=http://dreamatico.com/cat.html&h=1600&w=2560&tbnid=krrt7OEGWrXF5M:&docid=uM9xYXupwP9z-M&ei=JoQ3VvPDFYX9Up_fuJgG&tbm=isch&ved=0CDUQMygAMABqFQoTCPPuyOGJ8sgCFYW-FAodny8OYw'
-    },
-    {
-        title:'Another cat', src: 'http://www.google.nl/imgres?imgurl=https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg&imgrefurl=https://twitter.com/cats&h=640&w=638&tbnid=W79MI-OOcqCHsM:&docid=6ePtmzlTUw5vJM&ei=JoQ3VvPDFYX9Up_fuJgG&tbm=isch&ved=0CDcQMygCMAJqFQoTCPPuyOGJ8sgCFYW-FAodny8OYw'
-    }
-];
-
-var selectCat = function() {
-    var catSource = this.src;
-    console.log(this);
+var Cat = function(title, src) {
+  this.title = title;
+  this.src = src;
+  this.count = 0;
 };
 
-function populateCatsList(){
-    var catsList = document.getElementById('cats-list');
-    cats.forEach(function (cat) {
+Cat.prototype.constructor = Cat;
+Cat.prototype.getCount = function() {
+  return this.count;
+};
+Cat.prototype.incrementCount = function() {
+  this.count++;
+};
+
+var runApp = (function() {
+  var model = {
+    cats : [ new Cat('Cat 1', 'http://dreamatico.com/data_images/cat/cat-1.jpg'),
+        new Cat('Cat 2', 'http://dreamatico.com/data_images/cat/cat-2.jpg'),
+        new Cat('Cat 3', 'http://dreamatico.com/data_images/cat/cat-3.jpg'),
+        new Cat('Cat 4', 'http://dreamatico.com/data_images/cat/cat-4.jpg'),
+        new Cat('Cat 5', 'http://dreamatico.com/data_images/cat/cat-5.jpg'), ],
+
+  };
+  var controller = {
+
+    init : function() {
+      controller.selectCat.call(model.cats[0]);
+    },
+
+    selectCat : function() {
+      model.selectedCat = this;
+      controller.renderAll();
+    },
+
+    renderAll : function() {
+      views.forEach(function(view) {
+        view.render();
+      });
+    },
+
+    getSelectedCat : function() {
+      return model.selectedCat;
+    },
+
+    getCats : function() {
+      return model.cats;
+    },
+
+    catClicked : function() {
+      this.incrementCount();
+      controller.renderAll();
+    }
+
+  };
+
+  var views = [ {
+    render : function() {
+      var catsList = document.getElementById('cats-list');
+      catsList.innerHTML = '';
+      controller.getCats().forEach(function(cat) {
         var c = document.createElement('div');
         c.innerHTML = cat.title;
-        c.onclick = selectCat.bind(cat);
+        c.onclick = controller.selectCat.bind(cat);
         catsList.appendChild(c);
-    });
-}
+      });
+    }
+  }, {
+    render : function() {
+      var img = document.getElementById("cat-image");
+      var counter = document.getElementById("counter");
+      if (controller.getSelectedCat()) {
+        img.src = controller.getSelectedCat().src;
+        img.onclick = controller.catClicked.bind(controller.getSelectedCat());
+        counter.innerHTML = controller.getSelectedCat().getCount();
+      } else {
+        img.src = '';
+        counter.innerHTML = '';
+      }
+    }
+  } ];
+
+  controller.init();
+});
