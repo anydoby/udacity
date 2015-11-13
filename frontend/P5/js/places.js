f93018d6bb59c94187052f6122bde4614341d5aa
anydoby <anydoby@gmail.com>
2015-11-13 16:49:37 +0100

    adding scaffolding. need to figure out how to position the map on my locations

---
+++ frontend/P5/js/places.js
@@ -0,0 +1,29 @@
+var app = app || {};
+
+(function() {
+  app.rawData = {
+    city : 'Amsterdam NL',
+    locations : [
+     'Café-restaurant van Kerkwijk',
+     'Restaurant Azmarino',
+     'Singel 404',
+     'Restaurant De Kas',
+     'Getto Food & Drinks',
+     '&samhoud places',
+     'Hungry Birds Street Food Tours Amsterdam',
+     'Pancakes Amsterdam',
+     'Tomo Sushi',
+     'Eat Mode',
+     'Balthazar\'s Keuken',
+     'Mangetsu',
+     'La Perla Restaurant',
+     'Oriental City B.V.',
+     'Bazar Amsterdam',
+     'Castell',
+     'Broodje Bert',
+     'Greenwoods',
+     'Restaurant Greetje',
+     'Moeders'
+     ]
+  };
+})();
