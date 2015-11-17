ceba81579e76309adbd8fff00a5e884d8e01e930
anydoby <anydoby@gmail.com>
2015-11-18 00:17:31 +0100

    search/filter is now working. need to add search by place description.

--- frontend/P5/js/app.js
+++ frontend/P5/js/app.js
@@ -1,30 +1,40 @@
 var app = app || {};
 
 /*
- * represents input filter query and the resulting locations that match the query
+ * represents input filter query and the resulting locations that match the
+ * query
  */
 var SearchViewModel = function(app) {
   this.queryText = ko.observable();
 
   /*
-   * Results holds locations filtered by text query. If there's no query text everything is returned.
+   * Results holds locations filtered by text query. If there's no query text
+   * everything is returned.
    */
   this.locations = ko.computed(function() {
-    var queryText=this.queryText();
+    var queryText = this.queryText();
     if (!queryText || queryText.trim().length == 0) {
       return app.locations();
     }
 
-    var queryTerms = queryText.slice(' ');
-    return app.locations().filter(function(location) {
-      for (i in queryTerms) {
-        if (location.title().indexOf(queryTerms[i]) != -1) {
+    var queryTerms = queryText.toLowerCase().split(' ');
+    var filtered = app.locations().filter(function(location) {
+      for (var i = 0; i < queryTerms.length; i++) {
+        var term = queryTerms[i];
+        if (location.title().toLowerCase().indexOf(term) != -1) {
           return true;
         }
       }
       return false;
     });
+    return filtered;
   }, this);
+  var hide = function(location){location.visible(false);};
+  var show = function(location){location.visible(true);};
+  this.locations.subscribe(function(newLocations){
+    app.locations().forEach(hide);
+    newLocations.forEach(show);
+  });
 };
 
 $(function() {
